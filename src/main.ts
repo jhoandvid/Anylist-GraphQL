510dd21bc3422ca18a0c769e63c22033d2fa12ab
jhoandvid <rojasalmariojhoandvid@gmail.com>
2023-02-07 09:41:57 -0500

    config init

--- src/main.ts
+++ src/main.ts
@@ -6,9 +6,12 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({
     whitelist:true,
-    forbidNonWhitelisted:true
+    //forbidNonWhitelisted:true
   }))
   
-  await app.listen(3000);
+  const PORT = process.env.PORT || 3000;
+
+  await app.listen(PORT);
+  console.log(`App running on port ${ PORT }`);
 }
 bootstrap();
